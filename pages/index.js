674f54e7264d4f90df48eb72c0df8ab9e6c08229
Alex <64358227+ARCIKE@users.noreply.github.com>
2022-10-03 10:11:02 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome !" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         IP Minecraft <code>mc.info1d.gq</code>
         </p>
       </main>
     </div>
